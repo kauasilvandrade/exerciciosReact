a41cb992c8f144d69a3f720a776dbc01ea2d7bc2
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2026-03-12 12:30:41 -0300

    adding localStorage

--- lista/src/App.tsx
+++ lista/src/App.tsx
@@ -1,61 +1,71 @@
-  import "./global.css"
+import "./global.css"
 
-  import { useState } from "react"
+import { useState, useEffect } from "react"
 
-  import { InputText } from "./components/InputText"
-  import { Button } from "./components/Button"
-  import { Card } from "./components/Card"
+import { InputText } from "./components/InputText"
+import { Button } from "./components/Button"
+import { Card } from "./components/Card"
 
-  import styles from "./app.module.css"
+import styles from "./app.module.css"
 
-  export function App() {
-    
-    type Task = {
-      id: number, 
-      name: string,
+type Task = {
+  id: number, 
+  name: string,
+}
+
+export function App() {
+  
+  const [taskName, setTaskName] = useState("")
+
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const tasksStorage = localStorage.getItem("tasks")
+    return tasksStorage ? (JSON.parse(tasksStorage) as Task[]) : []
+  })
+
+  function addNewTasks() {
+
+    if (!taskName.trim()) return  
+
+    const newTask = {
+      id: Date.now(),
+      name: taskName,
     }
 
-    const [taskName, setTaskName] = useState("")
-    const [tasks, setTasks] = useState<Task[]>([])
+    setTasks((prevState) => [...prevState, newTask])
 
-    function addNewTasks() {
+    setTaskName("")
+  }
 
-      const newTask = {
-        id: Date.now(),
-        name: taskName,
-      }
+  function removeTask(id: number) {
+    setTasks((prevState) => prevState.filter(task => task.id !== id))
+  }
 
-      setTasks((prevState) => [...prevState, newTask])
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
 
-      setTaskName("")
-    }
+  return (
+    <div className={styles.container}>
 
-    function removeTask(id: number) {
-      setTasks((prevState) => prevState.filter(task => task.id !== id))
-    }
+      <h1>Lista de Tarefas</h1>
 
-    return (
-      <div className={styles.container}>
+      <div className={styles.container__createTask}>
+        
+        <InputText value={taskName} placeholder="Digite uma tarefa" onChange={(e) => setTaskName(e.target.value)} />
 
-        <h1>Lista de Tarefas</h1>
-
-        <div className={styles.container__createTask}>
-          
-          <InputText value={taskName} placeholder="Digite uma tarefa" onChange={(e) => setTaskName(e.target.value)} />
-
-          <Button name="Adicionar" onClick={addNewTasks}/>
-
-        </div>
-
-        {
-          tasks.map((task) => 
-          <Card 
-          key={task.id} 
-          name={task.name}
-          onRemove={() => removeTask(task.id)}
-          />)
-        }
+        <Button name="Adicionar" onClick={addNewTasks}/>
 
       </div>
-    )
-  }
+
+      {
+        tasks.map((task) => 
+        <Card 
+        key={task.id} 
+        name={task.name}
+        onRemove={() => removeTask(task.id)}
+        />)
+      }
+
+    </div>
+  )
+}
